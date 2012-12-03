c43f7a07c57ff028090006c19d6a249813c0424e
SeriousM <masta2ooo@gmail.com>
2012-12-03 11:32:47 +0000

    initial commit;

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuackingNemesis::Application.config.secret_token = '4ab0d7ce8252e7f468f348806eec405a6c2f0aefab4aa9140a57b583411882b05dc024e20519c2a0244e66510e5a5ccb33a56d90d7cfba09613f02b0fa8e24ec'
