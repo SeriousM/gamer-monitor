d8d0193eee915c1666641823200ac089c2e099ff
SeriousM <masta2ooo@gmail.com>
2012-12-09 21:24:03 +0000

    made small changes to the environment files;

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,10 +22,13 @@ QuackingNemesis::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-
   # Do not compress assets
   config.assets.compress = false
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # set the mongoid logging
+  Mongoid.logger.level = Logger::DEBUG
+  Moped.logger.level = Logger::DEBUG
 end
