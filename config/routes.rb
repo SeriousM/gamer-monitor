0e6c304a5b7248833a218e1d1620170b1762b8d4
SeriousM <masta2ooo@gmail.com>
2012-12-13 13:42:11 +0000

    Removed the user edit page becuase this is handled on the index page;

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 QuackingNemesis::Application.routes.draw do
   root :to => "home#index"
-  resources :users, :only => [:index, :show, :edit, :update ]
+  resources :users, :only => [:index, :show, :update ]
   match '/auth/:provider/callback' => 'sessions#create'
   match '/signin' => 'sessions#new', :as => :signin
   match '/signout' => 'sessions#destroy', :as => :signout
