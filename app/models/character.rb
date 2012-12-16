f9cbbc99c26fff63dda113a921b49cc66af3aaca
SeriousM <masta2ooo@gmail.com>
2012-12-16 02:00:28 +0000

    Added the Game and Character classes that are related to the User; A WowCharacter class was also created for the first game implementation;

---
+++ app/models/character.rb
@@ -0,0 +1,17 @@
+require 'symbolize/mongoid'
+
+class Character
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  belongs_to :game
+  belongs_to :user
+end
+
+class WowCharacter < Character
+  field :server, type: String
+  field :name, type: String
+  
+  attr_accessible :name, :server
+  validates_presence_of :name, :server
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,28 @@
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
+  
+  has_and_belongs_to_many :games
+  has_many :characters do
+    def for_wow
+      @target.select { |c| c._type == "WowCharacter"}
+    end
+  end
+  
   rolify
+  
   field :provider, type: String
   field :uid, type: String
   field :name, type: String
   field :email, type: String
+  
   attr_accessible :role_ids, :as => :admin
   attr_accessible :provider, :uid, :name, :email
-  # run 'rake db:mongoid:create_indexes' to create indexes
+  
   index({ uid: 1, provider: 1 }, { unique: true, background: true })
+  
+  validates_presence_of :provider, :uid, :name, :email
+  validates_uniqueness_of :uid
 
   def self.create_with_omniauth(auth)
     create! do |user|
@@ -17,8 +30,8 @@ class User
       user.uid = auth['uid']
       if auth['info']
         # todo: raise an error if the name or email is missing
-        user.name = auth['info']['name'] || ""
-        user.email = auth['info']['email'] || ""
+        user.name = auth['info']['name']
+        user.email = auth['info']['email']
       end
     end
   end
