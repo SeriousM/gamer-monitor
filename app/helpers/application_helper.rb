cde29db2d268f6aff46724287cf2f9d6bb6b4c50
SeriousM <masta2ooo@gmail.com>
2012-12-19 23:35:03 +0100

    Added a helper function to obfuscate email addresses with html characters (http://bit.ly/RENpBy); Added contact information to the contact.html.haml file;

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,15 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  def obscure_email(email)
+    return nil if email.nil? #Don't bother if the parameter is nil.
+    lower = ('a'..'z').to_a
+    upper = ('A'..'Z').to_a
+    email.split('').map { |char|
+        output = lower.index(char) + 97 if lower.include?(char)
+        output = upper.index(char) + 65 if upper.include?(char)
+        output ? "&##{output};" : (char == '@' ? '&#0064;' : char)
+    }.join
+  end
 end
