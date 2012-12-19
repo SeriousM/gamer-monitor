aa1b1322ffd08a7b4dc5c9bb74810ced0ac589d2
SeriousM <masta2ooo@gmail.com>
2012-12-19 10:10:32 +0000

    Disabled the environment initialization on precompilation;

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,10 @@ module QuackingNemesis
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
+    
+    # disable the initialization on precompilation as suggested on
+    # https://devcenter.heroku.com/articles/rails3x-asset-pipeline-cedar#troubleshooting
+    config.assets.initialize_on_precompile = false
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
